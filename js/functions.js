b032d949608f055a2302b5ad6b5649d01d081708
Yixing Cheng <waldenlaker@gmail.com>
2017-11-19 18:34:30 -0500

    update

--- js/functions.js
+++ js/functions.js
@@ -25,3 +25,74 @@ function filter(array, predicate) {
 	}
 	return result
 }
+
+/***************** Native map ****************************/
+
+Array.prototype.myMap = function(callback, context) {
+	var arry = []
+	for (var i = 0; i < this.length; i++) {
+		arr.push(callback(this[i], i, this))
+	}
+	return arr
+}
+
+
+/***************** Lodash map  ****************************/
+
+function map(array, iteratee) {
+	let index = -1
+	const length = array == null ? 0 : array.length
+	const result = new Array(length)
+
+	while(++index < length) {
+		result[index] = iteratee(array[index], index, array)
+	}
+	return result;
+}
+
+
+/**************** Native reduce ****************************/
+
+Array.prototype.myReduce = function(callback, initialVal) {
+	var accumulator = (initialVal === undefined) ? undefined : initialVal
+	for (var i = 0; i < this.length; i++) {
+		if (accumulator !== undefined) {
+			accumulator = callback.call(undefined, accumulator, this[i], i, this);
+		} else
+			accumulator = this[i]
+	}
+	return accumulator
+}
+
+
+/**************** Lodash reduce ****************************/
+
+function reduce(collection, iteratee, accumulator) {
+	const func = Array.isArray(collection) ? arrayReduce : baseReduce
+	const initAccum = arguments.length < 3
+	return func(collection, iteratee, accumulator, initAccum, baseEach)
+}
+
+function arrayReduce(array, iteratee, accumulator, initAccum) {
+	let index = -1
+	const length = array == null ? 0 : array.length
+
+	if (initAccum && length) {
+		accumulator = array[++index]
+	}
+	while(++index < length) {
+		accumulator = iteratee(accumulator, array[index], index, array)
+	}
+	return accumulator
+}
+
+function baseReduce(collection, iteratee, accumulator, initAccum, eachFunc) {
+	eachFunc(collection, (value, index, collection) => {
+		accumulator = initAccum ? (initAccum = false, value) : iteratee(accumulator, value, index, collection)
+	})
+	return accumulator
+}
+
+
+
+
